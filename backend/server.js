046d0370bcefa1663fa134f0e676de553da92114
Jonatan Rodrigues de Souza <jonatan.rodrigues@pca.com.br>
2020-04-19 16:45:52 -0300

    inicio frontend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,28 @@ const express = require('express');
 
 const app = express();
 
+var lista1 = ["abc", "bca", "cab"];
+var lista2 = ['xyz', 'zyx', 'yzx'];
+
+var result = [...lista1, ...lista2];
+console.log(result);
+
+var json = { 
+    "nome": "jonzera b.o da ZL",
+    "idade": 22,
+    "skills": {
+        "web": ["nodejs", "ASP.NET", "SpringBoot"]
+    }
+}
+
+function printJson({ nome, ...resto }){
+    console.log(nome, resto);
+}
+
+printJson(json);
+
+
+
 app.get('/', (req, res) => {
     res.json({ 
         "nome": "jonzera b.o da ZL",
